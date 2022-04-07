9b2bff6a2305c7e67a1adaf7fe5003ee510eb88e
nas11ai <74126181+nas11ai@users.noreply.github.com>
2022-04-07 15:39:13 +0800

    2.6: the phonebook step 1

---
+++ phonebook/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleNameSubmit = (event) => {
+    event.preventDefault()
+    const newNameObj = { name: newName }
+    setPersons(persons.concat(newNameObj))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleNameSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(item => <li key={item.name}>{item.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
