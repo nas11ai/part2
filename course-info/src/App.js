9642f5ca2774fc03668def5fc6bc653b1043f939
nas11ai <74126181+nas11ai@users.noreply.github.com>
2022-04-07 12:51:50 +0800

    2.5: separate module

--- course-info/src/App.js
+++ course-info/src/App.js
@@ -1,34 +1,4 @@
-const Header = ({ course }) => <h1 key={course.id}>{course.name}</h1>
-
-const Total = ({ course }) => (
-  <p key={course.id}>
-    <strong>
-      total of {course.parts.reduce((total, item) => total += item.exercises, 0)} exercises
-    </strong>
-  </p>
-)
-
-const Part = ({ part }) =>
-  <p key={part.id}>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ course }) => (
-  <div key={course.id}>
-    {course.parts.map(x => <Part part={x} />)}
-  </div>
-)
-
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
+import Course from './component/Course'
 
 const App = () => {
   const courses = [
